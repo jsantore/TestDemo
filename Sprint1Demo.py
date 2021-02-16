91e17d7b5a0d53fbe9c547c689df89f4013902a0
jsantore <jsantore@bridgew.edu>
2021-02-16 08:11:00 -0500

    debugging run for secrets

--- Sprint1Demo.py
+++ Sprint1Demo.py
@@ -9,6 +9,10 @@ def get_data():
         f'https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,2018.student.size,'
         f'2016.repayment.3_yr_repayment.overall,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line&api_key={secrets.api_key}')
     first_page = response.json()
+    if response.status_code != 200:
+        print(f"error getting data!: key = {secrets.api_key} error: {response.raw}")  # get rid of this after debugging
+        return []
+
     total_results = first_page['metadata']['total']
     page = 0
     per_page = first_page['metadata']['per_page']
@@ -18,6 +22,8 @@ def get_data():
         response = requests.get(
             f'https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,2018.student.size,'
             f'2016.repayment.3_yr_repayment.overall,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line&api_key={secrets.api_key}&page={page}')
+        if response.status_code != 200:  # if we didn't get good data keep going
+            continue
         current_page = response.json()
         all_data.extend(current_page['results'])
 
